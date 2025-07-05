48ba31e2687ad8a0515f9b7f4808c34b1126e8b1
Bera Dhaval <dhaval.bera@ethicsinfotech.in>
2025-07-05 07:03:31 +0530

    refactor form layout and enhance styling; update backend request handling

--- frontend/app.js
+++ frontend/app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 const app = express();
 const PORT = 3000;
+const qs = require('qs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
@@ -13,9 +14,17 @@ app.get('/', (req, res) => {
 
 app.post('/submit', async (req, res) => {
     try {
-        const response = await axios.post('http://backend:5000/submit', req.body);
+        const response = await axios.post(
+            'http://backend:5000/submit',
+            qs.stringify(req.body),  // convert to x-www-form-urlencoded
+            {
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+            }
+        );
         res.send(`Response from backend: ${JSON.stringify(response.data)}`);
     } catch (err) {
+        console.log(err);
+
         res.status(500).send('Error communicating with backend');
     }
 });
